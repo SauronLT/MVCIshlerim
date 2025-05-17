bbca5cc1527dde89c89c228b595ce6f22b1f5e31
qara <ASUS@DESKTOP-VI89K5E>
2025-05-17 18:39:27 +0400

    salamlar

---
+++ Library/Library/book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library;
+
+public class Book
+{
+    private static int _idCounter = 0;
+
+    public int Id { get; private set; }
+    public string Name { get; set; }
+    public string AuthorName { get; set; }
+    public double Price { get; set; }
+
+    public Book(string name, string authorName, double price)
+    {
+        Id = ++_idCounter;
+        Name = name;
+        AuthorName = authorName;
+        Price = price;
+    }
+
+    public string ShowInfo()
+    {
+        return "Id: " + Id + ", Name: " + Name + ", Author: " + AuthorName + ", Price: " + Price;
+    }
+
+    public override string ToString()
+    {
+        return ShowInfo();
+    }
+}
---
+++ Library/Library/library.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library;
+
+public class Library
+{
+    public string Name { get; set; }
+    private Book[] books = new Book[0];
+
+    public void AddBook(Book book)
+    {
+        Array.Resize(ref books, books.Length + 1);
+        books[books.Length-1] = book;
+    }
+
+    public Book GetBookById(int id)
+    {
+        return books.FirstOrDefault(b => b.Id == id);
+    }
+
+    
+
+    public Book[] GetBook(string name)
+    {
+        return books.Where(b => b.Name.ToLower().Contains(name.ToLower())).ToArray();
+    }
+
+    public void GetAllBooks()
+    {
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
+    }
+
+    public void Update(int id, Book newBook)
+    {
+        var existing = GetBookById(id);
+        if (existing != null)
+        {
+            existing.Name = newBook.Name;
+            existing.AuthorName = newBook.AuthorName;
+            existing.Price = newBook.Price;
+        }
+    }
+}
